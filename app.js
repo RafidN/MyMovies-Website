396f64eecc48fb0890b19b6ded1d35b2c61252b3
RafidN <rafidn@outlook.com>
2023-06-07 18:04:14 -0500

    final build 6/7/2023

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const movieListEl = document.querySelector(".movies")
 
 async function onSearchChange(event){
     movieName = event.target.value
-    const movies = await fetch(`https://www.omdbapi.com/?s=${movieName || "apple"}&page=1&apikey=cfc93c83`);
+    const movies = await fetch(`https://www.omdbapi.com/?s=${movieName}&page=1&apikey=cfc93c83`);
     const moviesData = await movies.json();
+    if(moviesData.Response == "True"){
+        clearBox();
+    }
     for(let i=0; i<3; i++) {
         const movieId = moviesData.Search[i].imdbID
         const moviewait = await fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=cfc93c83`);
@@ -19,9 +22,12 @@ async function onSearchChange(event){
 }
 
 function movieHTML(movieData) {
+    // if(movieData.Poster == "N/A") { "https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg"
+        
+    // }
     movieListEl.innerHTML += `<li class="movie">
         <div class="movie__wrapper">
-            <img src="${movieData.Poster}" alt="" class="movie__img"> 
+            <img src="${movieData.Poster=="N/A"? "https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg" : movieData.Poster}" alt="" class="movie__img"> 
 
             <div class="movie__description">
                 <p class="movie__title">${movieData.Title}</p>
@@ -30,20 +36,24 @@ function movieHTML(movieData) {
                 <div class="movie__ratings">
                     <div class="movie__rating">
                         <div class="movie__rating--title">IMDB</div>
-                        <div class="movie__rating--score">1</div>
-                    </div>
-
-                    <div class="movie__rating">
-                        <div class="movie__rating--title">Rotten Tomatoes</div>
-                        <div class="movie__rating--score">2</div>
+                        <div class="movie__rating--score">${movieData.imdbRating}</div>
                     </div>
 
                     <div class="movie__rating">
                         <div class="movie__rating--title">Metacritic</div>
-                        <div class="movie__rating--score">3</div>
+                        <div class="movie__rating--score">${movieData.Metascore!="N/A"? movieData.Metascore + "/100": "N/A"}</div>
+                    </div>
+
+                    <div class="movie__rating">
+                        <div class="movie__rating--title">Box Office</div>
+                        <div class="movie__rating--score">${movieData.BoxOffice}</div>
                     </div>
                 </div>
             </div>
         </div>
     </li>`
+}
+
+function clearBox() {
+    movieListEl.innerHTML = "";
 }
